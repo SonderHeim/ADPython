5a471d0ba6431182e57d91a3e947fefa2e8ed4fb
SonderHeim <ma24722222@ya.ru>
2022-09-19 19:07:56 +0300

    Добавил background в веб-приложение.

--- my.py
+++ my.py
@@ -1,9 +1,27 @@
 # Запуск через команду в консоль: streamlit run my.py
 # Использовал PyCharm как IDE.
 import io
+import streamlit.components.v1 as components
 import streamlit as st
 from PIL import Image
 
+# components.html(
+#     """
+#     <link rel="stylesheet" href="static/style.css" type="text/css"/>
+#     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+#     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+#     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+#     <body style="start"></body>
+#     """,
+# )
+
+st.markdown("""
+<style>
+start {
+  padding-top: 80px;
+}
+</style>
+""", unsafe_allow_html=True)
 
 def load_image():
     # Форма для загрузки изображения
